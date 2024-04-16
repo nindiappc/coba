32b524a87fb8d104cfd6e5a9081e52a4716efdaf
nindiappc <148948592+nindiappc@users.noreply.github.com>
2024-04-16 13:29:00 +0700

    Add files via upload

---
+++ stay.js
@@ -0,0 +1,9 @@
+var x = 5;
+let name = 'nindia';
+let y = 6;
+alert('haii')
+function dataDiri() {
+    let name = document.getElementById('name').value;
+    let alamat = document.getElementById('alamat').value;
+    document.getElementById('coba').innerHTML = name + " " + alamat;
+}
